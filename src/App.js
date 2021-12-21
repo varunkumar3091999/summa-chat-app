f63a1dfeb5414171d7a34542ea1dd0aed8804810
Varun Kumar <varun@apples-MacBook-Air.local>
2021-12-21 14:56:43 +0530

    redirect to sign in when not authenticated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
 import Signup from "./screens/Signup";
 import Signin from "./screens/Signin";
 import Home from "./screens/Home";
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(getAuth(), (user) => {
+      if (user) {
+        setAuthenticated(true);
+        console.log(user);
+      } else {
+        setAuthenticated(false);
+        console.log(user);
+      }
+    });
+  }, []);
+
   return (
     <div className="App h-screen ">
-      <Router>
-        <Routes>
-          {console.log(process.env)}
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      {authenticated ? (
+        <Router>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="*" element={<Signin />} />
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
