f137e53de98b8cc4174ce772750f447a2240cb5e
Varun Kumar <varun@apples-MacBook-Air.local>
2021-12-27 18:01:12 +0530

    bug fix - set default loading state to false in signup page

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -11,7 +11,7 @@ const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const db = getFirestore(app);
 
